bb15c6b663ab06b946fad7587234cc88528706ba
Moha Hamra <mohah3000@gmail.com>
2025-09-17 12:43:23 +0200

    Creado clase bus

---
+++ bus.py
@@ -0,0 +1,28 @@
+class Bus:
+    bus_id = 0
+
+    def __init__(self, plazas):
+        Bus.bus_id += 1
+        self.plazas = plazas
+        self.plazas_iniciales = plazas
+
+    def ventaBilletes(self, plazas, billetes):
+        if(billetes > plazas):
+            return None
+        else:
+            return plazas - billetes
+        
+    def devolucionBilletes(self, plazas, plazas_iniciales, billetes):
+        if(billetes > (plazas_iniciales - plazas)):
+            return None
+        else:
+            return plazas + billetes
+        
+    def plazasVendidas(self, plazas_iniciales, plazas):
+        return plazas_iniciales - plazas
+    
+    def estado(self):
+        plazas_vendidas = plazas_vendidas(self.plazas_iniciales, self.plazas)
+        print(f'Quedan {self.plazas} plazas de {self.plazas_iniciales} iniciales y se han vendido {plazas_vendidas} billetes.')
+        
+
